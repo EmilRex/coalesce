24bd8224a5aefd9fbcee8bebc919a3df957511cd
Emil Christensen <emil.rex.christensen@gmail.com>
2022-04-05 09:25:29 -0400

    Adds Python requirements and metadata

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="coalesce",
+    version="1.0.0",
+    description="",
+    url="https://github.com/EmilRex/coalesce",
+    author="Emil Christensen",
+    author_email="emil.rex.christensen@gmail.com",
+    packages=find_packages("."),
+)
